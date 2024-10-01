ff238f41d77a866231064d247f3ce8a3291ad2b4
Jon <jonhoganbs@gmail.com>
2024-09-30 21:13:55 -0500

    Initial Push

---
+++ name_gen/main.py
@@ -0,0 +1,9 @@
+import sys, random
+
+print('Welcome to the "Psych" Sidekick Name Generator.')
+
+first = ('Baby oil', 'Bad News', 'Big Burps',
+         'Bill "Beenie-Weenie"', 'Bob "Stinkbug"',
+         'Bowel Noises', 'Boxelder', 'Bud "Lite"',
+         'Butterbean', 'Buttermilk', 'Buttocks',
+         'Chad', 'Chesterfield', 'Chewy')
