47a7be83e843615963157a73e191d469c8fb6dae
HoganTechBytes <jonhoganbs@gmail.com>
2024-10-09 17:19:08 -0500

    initial push

---
+++ band_name_gen/main.py
@@ -0,0 +1,6 @@
+'''Simple program to create a band name using your birth city, and pet's name'''
+
+city_name = input("Enter the name of the city you were born in: ")
+pet_name = input("Enter your pet's name: ")
+
+print(f'Your band\'s name is {city_name} {pet_name}')
