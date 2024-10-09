8e9e2bb07cc29daf9827c244ae0b58a4e5480776
HoganTechBytes <jonhoganbs@gmail.com>
2024-10-09 16:55:32 -0500

    Update

--- hello_world/main.py
+++ hello_world/main.py
@@ -1,3 +1,20 @@
 print("Hello world using three print statements:")
 print("Hello world!")
 print("Hello world!")
+print("Hello world!\n")
+
+print("Hello world using one print statement and '\\n':")
+print("Hello world!\nHello world!\nHello World!\n")
+
+print("Hello world using one print statement and sting multiplication:")
+print(3 * "Hello world!\n")
+
+print("Hello world using a for loop:")
+for num in range(3):
+    print("Hello world!")
+
+print("\nHello world using a while loop:")
+num = 0
+while num < 3:
+    print("Hello world!")
+    num += 1
