5218f9282538b9afdcc757e165113398f3db9699
HoganTechBytes <jonhoganbs@gmail.com>
2024-10-12 12:39:11 -0500

    Updated player guess input and changed error messages

--- hangman/ValidateGuess.py
+++ hangman/ValidateGuess.py
@@ -9,14 +9,14 @@
         string
 '''
 
-def ValidateGuess():
+def ValidateGuess() -> str:
     while True:
-        player_guess = input("Enter a letter: ")
+        player_guess = input("Enter a letter: ").lower()
 
         if len(player_guess) != 1:
-            print("Invalid guess, please try again")
-        elif player_guess.isalpha() == False:
-            print("Your guess must be a letter")
+            print("Invalid guess. Please enter a single letter.")
+        elif not player_guess.isalpha():
+            print("Invalid input. Your guess must be a letter.")
         else:
             return player_guess
         
