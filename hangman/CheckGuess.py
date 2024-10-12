d9803530015f7f6f912bf20d70fe90c7f5dcc9fb
HoganTechBytes <jonhoganbs@gmail.com>
2024-10-12 13:23:41 -0500

    Updated to track lives/tries and return a tuple

--- hangman/CheckGuess.py
+++ hangman/CheckGuess.py
@@ -7,10 +7,10 @@
         hidden_word (str): String for the secret word with guessed letters
 
     Returns:
-        string
+        tuple
 '''
 
-def CheckGuess(player_guess: str, secret_word: str, hidden_word: str) -> str:
+def CheckGuess(player_guess: str, secret_word: str, hidden_word: str, lives) -> tuple:
     #Check player guessed letter versus the hidden word to see if there is a match
 
     new_hidden_word = ""
@@ -19,6 +19,9 @@ def CheckGuess(player_guess: str, secret_word: str, hidden_word: str) -> str:
             new_hidden_word += player_guess
         else:
             new_hidden_word += hidden_word[letter]
+        
+    if hidden_word == new_hidden_word:
+        lives -= 1
     
-    return (new_hidden_word)
+    return (new_hidden_word, lives)
 
