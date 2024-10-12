d5a0fbe0c8a65f595e0d681f7b1fec474f082e22
HoganTechBytes <jonhoganbs@gmail.com>
2024-10-12 12:40:50 -0500

    Updated for added functions

--- hangman/main.py
+++ hangman/main.py
@@ -1,12 +1,16 @@
 '''A hangman game with a list of secret words'''
+import os
 import GetSecret
 import HideWord
 import ValidateGuess
+import Noose
+import CheckGuess
+
 
 
 #main game loop
 playing = True
-lives = 5
+lives = 6
 
 while playing:
     secret = GetSecret.GetSecret()
@@ -17,8 +21,27 @@ while playing:
     #new game loop
     word_guessed = False
     while not word_guessed:
-        
+        #Clear the console
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("""
+ 
+                 /$$   /$$                                                                
+                | $$  | $$                                                                
+                | $$  | $$  /$$$$$$  /$$$$$$$   /$$$$$$  /$$$$$$/$$$$   /$$$$$$  /$$$$$$$ 
+                | $$$$$$$$ |____  $$| $$__  $$ /$$__  $$| $$_  $$_  $$ |____  $$| $$__  $$
+                | $$__  $$  /$$$$$$$| $$  \ $$| $$  \ $$| $$ \ $$ \ $$  /$$$$$$$| $$  \ $$
+                | $$  | $$ /$$__  $$| $$  | $$| $$  | $$| $$ | $$ | $$ /$$__  $$| $$  | $$
+                | $$  | $$|  $$$$$$$| $$  | $$|  $$$$$$$| $$ | $$ | $$|  $$$$$$$| $$  | $$
+                |__/  |__/ \_______/|__/  |__/ \____  $$|__/ |__/ |__/ \_______/|__/  |__/
+                                               /$$  \ $$                                  
+                                              |  $$$$$$/                                  
+                                               \______/                                   
+ 
+""")
+
+        Noose.Noose_Pic(lives)
+        print(hidden_word)
         player_guess = ValidateGuess.ValidateGuess()
+        hidden_word = CheckGuess.CheckGuess(player_guess, secret, hidden_word)
+
         
-
-
